f27efef2a86dceab86e2f9784e474e5ec5325a1a
filip-urban <filipurban93@gmail.com>
2025-08-07 09:21:07 +0200

    add get_files_info schema

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.get_files_info import schema_get_files_info
+
 
 def main():
     load_dotenv()
@@ -12,14 +14,36 @@ def main():
     client = genai.Client(api_key=api_key)
     MODEL = "gemini-2.0-flash-001"
     prompt = sys.argv[1]
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+    config = types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
     response = client.models.generate_content(
         model=MODEL,
         contents=messages,
+        config=config,
     )
-    print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
     if len(sys.argv) > 2:
         if sys.argv[2] == "--verbose":
             print(f"User prompt: {prompt}")
