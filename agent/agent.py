a6af9f31df20e579cf9e57d229b1b6321e76dc1a
filip-urban <filipurban93@gmail.com>
2025-09-07 10:14:12 +0200

    clean unused modules

--- agent/agent.py
+++ agent/agent.py
@@ -1,13 +1,8 @@
 import sys
 
-from google import genai
 from google.genai import types
 
 from config import MODEL
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python import schema_run_python_file
-from functions.write_file import schema_write_file
 from functions.call_function import call_function
 
 
